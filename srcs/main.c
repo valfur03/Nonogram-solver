0a43a5b2a991b1f14efa48e98da19866368105e7
Valentin <valentin.furmanek@gmail.com>
2020-09-16 11:43:09 +0200

    Free memory

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: vfurmane <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/14 18:38:19 by vfurmane          #+#    #+#             */
-/*   Updated: 2020/09/15 18:47:52 by vfurmane         ###   ########.fr       */
+/*   Updated: 2020/09/16 11:11:25 by vfurmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,25 @@
 #include "check_args.h"
 #include "solve.h"
 #include <stdio.h> /* ===== DELETE ===== */
+
+void	ft_free_all(int ***args, int **board, int *size)
+{
+	int	i;
+
+	i = 0;
+	while (i < size[0])
+		free(board[i++]);
+	free(board);
+	i = 0;
+	while (i < size[0])
+		free(args[0][i++]);
+	i = 0;
+	while (i < size[1])
+		free(args[1][i++]);
+	free(args[0]);
+	free(args[1]);
+	free(size);
+}
 
 int		main(int argc, char **argv)
 {
@@ -27,9 +46,11 @@ int		main(int argc, char **argv)
 	args[1] = NULL;
 	if (!(size = ft_check_args(argv[1], args)))
 		return (0);
-	board = ft_init_board(size[0], size[1]);
+	if (!(board = ft_init_board(size[0], size[1])))
+		return (0);
 	if (!ft_solve(board, args, 0, 0, size, 0))
 		return (0);
 	ft_put_board(board, size);
+	ft_free_all(args, board, size);
 	return (0);
 }
