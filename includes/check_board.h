fed2a30dce097751e43a27a1fd3e5a2da1771a48
Valentin <valentin.furmanek@gmail.com>
2020-09-16 12:19:51 +0200

    Imporve the speed of the program

--- includes/check_board.h
+++ includes/check_board.h
@@ -6,12 +6,14 @@
 /*   By: vfurmane <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/15 08:59:22 by vfurmane          #+#    #+#             */
-/*   Updated: 2020/09/15 16:28:50 by vfurmane         ###   ########.fr       */
+/*   Updated: 2020/09/16 12:07:55 by vfurmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CHECK_BOARD_H
 # define CHECK_BOARD_H
+
+int	ft_precheck_board(int **board, int ***args, int *size);
 
 int	ft_check_board(int **board, int ***args, int *size);
 
--- srcs/check_board.c
+++ srcs/check_board.c
@@ -6,11 +6,41 @@
 /*   By: vfurmane <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/15 08:39:11 by vfurmane          #+#    #+#             */
-/*   Updated: 2020/09/15 16:27:24 by vfurmane         ###   ########.fr       */
+/*   Updated: 2020/09/16 12:07:44 by vfurmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "check_board.h"
+
+int	ft_precheck_board(int **board, int ***args, int *size)
+{
+	int	i;
+	int	j;
+	int	num;
+	int	count;
+
+	i = 0;
+	while (i < size[0])
+	{
+		num = 0;
+		j = 0;
+		while (args[0][i][num])
+		{
+			count = 0;
+			while (j < size[1] && board[j][i] == 0)
+				j++;
+			while (j < size[1] && board[j][i] == 1)
+			{
+				j++;
+				count++;
+			}
+			if (count > args[0][i][num++])
+				return (0);
+		}
+		i++;
+	}
+	return (1);
+}
 
 int	ft_check_board(int **board, int ***args, int *size)
 {
